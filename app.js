b20d8f902ee6b3ab394eb527b7760e16563b6932
Andrew <shifted6@gmail.com>
2020-01-13 18:40:04 -0800

    Initial file creation of index.html, sales.html, app.js, style.css, and README.md. First object creation.

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm']
+
+var storeSeattle = {
+    location: 'Seattle',
+    custMaxHr: 65,
+    custMinHr: 23,
+    cookiesAvgCust: 6.3,
+    genRandomCustomer: function(inHours) {
+        var cookieNum = 0;
+        var cookieString = '';
+
+        for (i=0; i < inHours.length; i++){
+            
+            cookieString = `${hours[i]}: 
+        }
+    }
+}
