4a889ef99e5ef21e613174c1e206bea3350e816f
Andrew <shifted6@gmail.com>
2020-01-14 23:08:01 -0800

    Completed table render functions, including prototype functions. Lab complete.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm']
 var storeData = [];
 var listSection = 'storeSales';
+var tableSection = 'storeSales';
 
 var Store = function(location, custMaxHr, custMinHr, cookiesAvgCust) {
     this.location = location;
@@ -28,7 +29,7 @@ Store.prototype.simCookies = function(inHours) {
     return this.cookieData;
 }
 
-Store.prototype.displayList = function(inHours, inCookies, sectionID) {
+Store.prototype.renderList = function(inHours, inCookies, sectionID) {
     var cookieString = '';
     var outputSection = document.getElementById(sectionID);
     var storeUL = document.createElement('ul');
@@ -46,8 +47,75 @@ Store.prototype.displayList = function(inHours, inCookies, sectionID) {
     outputSection.appendChild(storeUL);
 }
 
-Store.prototype.displayTable = function(data, sectionID) {
-    
+Store.prototype.render = function(table) {
+    //store location row header
+    var storeTR = document.createElement('tr');
+    var storeLocTH = document.createElement('th');
+    storeLocTH.textContent = this.location;
+    storeTR.appendChild(storeLocTH);
+    for (var j=0; j < hours.length; j++) {
+        //store row main
+        var storeTD = document.createElement('td');
+        storeTD.textContent = this.cookieData[j];
+        storeTR.appendChild(storeTD);
+    }
+    //store row footer totals
+    var storeTotal = this.cookieData[j];
+    var storeTotalTD = document.createElement('td')
+    storeTotalTD.textContent = storeTotal;
+    storeTR.appendChild(storeTotalTD);
+    table.appendChild(storeTR);
+}
+
+function renderAll(sectionID) {
+    var section = document.getElementById(sectionID);
+    var table = document.createElement('table');
+    renderHeader(table);
+    for (var i=0; i < storeData.length; i++) {
+        storeData[i].render(table);
+    }
+    renderFooter(table);
+    section.appendChild(table);
+}
+
+function renderHeader(table) {
+    var headerTR = document.createElement('tr');
+    //blank location column header
+    var blankTH = document.createElement('th');
+    headerTR.appendChild(blankTH);
+    for (var i=0; i<hours.length; i++) {
+        //hour main headers
+        var hourTH = document.createElement('th');
+        hourTH.textContent = hours[i];
+        headerTR.appendChild(hourTH);
+    }
+    //hour total header
+    var totalLocTH = document.createElement('th');
+    totalLocTH.textContent = 'Daily Location Total'
+    headerTR.appendChild(totalLocTH);
+    table.appendChild(headerTR);
+}
+
+function renderFooter(table) {
+    var grandTotal = 0;
+    var timeTotalRow = document.createElement('tr');
+    var timeTotalTH = document.createElement('th');
+    timeTotalTH.textContent = 'Totals';
+    timeTotalRow.appendChild(timeTotalTH);
+    for (var i=0; i < hours.length; i++) {
+        var timeTotal = 0;
+        var timeTotalTD = document.createElement('td');
+        for (var j=0; j < storeData.length;j++) {
+            timeTotal += storeData[j].cookieData[i];
+        }
+        timeTotalTD.textContent = timeTotal;
+        timeTotalRow.appendChild(timeTotalTD);
+        grandTotal += timeTotal;
+    }
+    var grandTotalTD = document.createElement('td');
+    grandTotalTD.textContent = grandTotal;
+    timeTotalRow.appendChild(grandTotalTD);
+    table.appendChild(timeTotalRow);
 }
 
 // storeSeattle.simCookies(hours, 'storeSales');
@@ -57,12 +125,10 @@ var storeDubai = new Store('Dubai', 38, 11, 3.7);
 var storeParis = new Store('Paris', 38, 20, 2.3);
 var storeLima = new Store('Lima', 2, 16, 4.6);
 storeSeattle.simCookies(hours);
-storeSeattle.displayList(hours, storeSeattle.cookieData, listSection);
 storeTokyo.simCookies(hours);
-storeTokyo.displayList(hours, storeTokyo.cookieData, listSection);
-
-// storeSeattle.displayList(hours,cookies,listSection);
-// cookies = storeTokyo.simCookies(hours);
-// storeTokyo.displayList(hours, cookies, listSection);
+storeDubai.simCookies(hours);
+storeParis.simCookies(hours);
+storeLima.simCookies(hours);
+renderAll(tableSection);
 
 
